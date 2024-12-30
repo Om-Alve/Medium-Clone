5e14973d152020797fd153d77ee2f673fa62684c
Om-Alve <omalve8174@gmail.com>
2024-12-30 10:12:59 +0530

    fixed signin issue

--- frontend/src/components/AuthHeader.tsx
+++ frontend/src/components/AuthHeader.tsx
@@ -4,7 +4,7 @@ export const AuthHeader = ({ type }: { type: "signin" | "signup" }) => {
   return (
     <div className="px-10">
       <div className="text-3xl font-extrabold">Create an Account</div>
-      <div className="text-slate-400 ml-5 mt-3">{type === "signin" ? "Already have an Account?" : "Don't have an account"} <Link className="underline pl-2" to={type === 'signin' ? '/signup' : '/signin'}>{type === 'signin' ? "Sign Up" : "Login"}</Link></div>
+      <div className="text-slate-400 ml-5 mt-3">{type === "signup" ? "Already have an Account?" : "Don't have an account"} <Link className="underline pl-2" to={type === 'signin' ? '/signup' : '/signin'}>{type === 'signin' ? "Sign Up" : "Login"}</Link></div>
     </div>
   )
 }
